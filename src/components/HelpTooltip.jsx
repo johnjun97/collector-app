3673793a68ed39aab7f736f03409eb607b395283
John <johnjun97@outlook.com>
2026-08-10 17:49:14 +0800

    cover in background in edit

--- src/components/HelpTooltip.jsx
+++ src/components/HelpTooltip.jsx
@@ -1,9 +1,20 @@
 import './HelpTooltip.css'
 
 export default function HelpTooltip({ children }) {
+    const handleClick = (e) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
     return (
-        <span className="help-tooltip">
-            <span className="help-tooltip-icon">!</span>
+        <span
+            className="help-tooltip"
+            onClick={handleClick}
+            onTouchStart={handleClick}
+        >
+            <span className="help-tooltip-icon">
+                ?
+            </span>
 
             <span className="help-tooltip-content">
                 {children}
