ffe900f64faf9a9ba9969992d8e41591ff99649f
johnjun <johnjun97@outlook.com>
2026-09-04 17:51:22 +0800

    add exp percentage

--- src/pages/home/ContributionCard/ContributionCard.jsx
+++ src/pages/home/ContributionCard/ContributionCard.jsx
@@ -9,21 +9,21 @@ export default function ContributionCard({ stats, levelInfo }) {
                     <h2>Level {levelInfo.level}</h2>
 
                     <span>
-    {levelInfo.nextLevelPoints - stats.total} EXP to next level
+                        {levelInfo.nextLevelPoints - stats.total} EXP to next level  ({levelInfo.progress}%)
                     </span>
                 </div>
             </div>
 
-<div className="contribution-progress">
-    <div className="contribution-progress-bar">
-        <div
-            className="contribution-progress-fill"
-            style={{
-                width: `${levelInfo.progress}%`
-            }}
-        />
-    </div>
-</div>
+            <div className="contribution-progress">
+                <div className="contribution-progress-bar">
+                    <div
+                        className="contribution-progress-fill"
+                        style={{
+                            width: `${levelInfo.progress}%`
+                        }}
+                    />
+                </div>
+            </div>
 
             <div className="contribution-stats">
                 <div className="contribution-stat">
